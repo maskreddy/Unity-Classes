0876ab40356e7d5eba0b81550318d3ae01c8bf27
maskreddy <59001051+maskreddy@users.noreply.github.com>
2021-11-19 14:02:52 +0530

    Shoot Update

---
+++ Assets/Scripts/06 Shooting/ShootPrjctl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootPrjctl : MonoBehaviour
+{
+
+    //Unity has 2 types of shooting: Projectiles & Ray casts
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
